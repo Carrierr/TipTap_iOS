cdfd94778f611a4f69a062f8bfd1a7bd4d4ea401
wjdgo813 <wjdgo813@naver.com>
2018-08-15 16:20:04 +0900

    정해현 [TipTap] - add alamofire, add swiftyJSON

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'TipTap' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'ScratchCard', '~> 1.1.3'
+  pod 'Alamofire', '~> 4.6'
+  pod 'SwiftyJSON'
   # Pods for TipTap
 
   target 'TipTapTests' do
--- Pods/Target Support Files/Pods-TipTap/Pods-TipTap-frameworks.sh
+++ Pods/Target Support Files/Pods-TipTap/Pods-TipTap-frameworks.sh
@@ -134,10 +134,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ScratchCard/ScratchCard.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ScratchCard/ScratchCard.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
