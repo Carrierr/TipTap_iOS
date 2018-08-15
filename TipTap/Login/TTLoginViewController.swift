c3b07c28baba965e500e401a9857147e54e22a48
wjdgo813 <wjdgo813@naver.com>
2018-08-15 16:10:52 +0900

    정해현 [TipTap] - complete scratch view, 			   complete base shared list

---
+++ TipTap/Login/TTLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TTLoginViewController.swift
+//  TipTap
+//
+//  Created by Haehyeon Jeong on 2018. 8. 15..
+//  Copyright © 2018년 Tiptap. All rights reserved.
+//
+
+import UIKit
+
+class TTLoginViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
